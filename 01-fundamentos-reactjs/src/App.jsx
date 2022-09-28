d5c0a439ffed49587b9cf2a55ba86631f48eab49
Danilo <danfonmar@gmail.com>
2022-09-28 11:29:18 -0300

    01-fundamentos-react

---
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -0,0 +1,20 @@
+import { Header } from "./components/Header"
+import { Post } from "./post"
+
+export function App() {
+
+  return (
+    <div>
+      <Header />
+
+      <Post 
+        author="Danilo Martins"
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium sequi minima expedita optio cumque in distinctio architecto fuga eum asperiores! Repellat fuga quidem, cupiditate consequuntur praesentium doloremque sunt repudiandae eum."
+      />
+      <Post 
+        author="Roberto Carlos"
+        content="Um novo post diferente"
+      />
+    </div>
+  )
+}
