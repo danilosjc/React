d568769932282645e1cc0750ecad4a282cadae33
danilosjc <danfonmar@gmail.com>
2022-10-06 10:13:26 -0300

    alterações no import

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -9,12 +9,12 @@ export function App() {
 
       <Post 
         author="Danilo Martins"
-        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Accusantium sequi minima expedita optio cumque in distinctio architecto fuga eum asperiores! Repellat fuga quidem, cupiditate consequuntur praesentium doloremque sunt repudiandae eum."
-      />
+        content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
+        Accusantium sequi minima expedita optio cumque in distinctio architecto 
+        fuga eum asperiores! Repellat fuga quidem, cupiditate consequuntur praesentium doloremque sunt repudiandae eum." />
       <Post 
         author="Roberto Carlos"
-        content="Um novo post diferente"
-      />
+        content="Um novo post diferente" />
     </div>
   )
 }
