0dfb4f0c24529baafef71291bde026c40be89629
danilosjc <danfonmar@gmail.com>
2022-10-13 11:15:05 -0300

    header finalizado

--- 01-fundamentos-reactjs/src/components/Header.jsx
+++ 01-fundamentos-reactjs/src/components/Header.jsx
@@ -1,9 +1,10 @@
 import styles from './Header.module.css'
+import igniteLogo from '../assets/Ignite-logo.svg'
 
 export function Header() {
     return (
         <header className={styles.header}>
-       <strong>ignite feed</strong>
+       <img src={igniteLogo} alt="logotipo" />
        </header>
     )
 }
