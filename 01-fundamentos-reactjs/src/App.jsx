2e50232d715aeefe35a4720ee8acd7bd37414100
danilosjc <danfonmar@gmail.com>
2022-10-21 12:30:59 -0300

    area de post contruida

--- 01-fundamentos-reactjs/src/App.jsx
+++ 01-fundamentos-reactjs/src/App.jsx
@@ -1,5 +1,5 @@
-import { Header } from "./components/Header"
-import { Post } from "./post"
+import { Header } from "./components/Header";
+import { Post } from "./components/Post";
 import styles from './App.module.css';
 
 import './global.css'
---
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -0,0 +1,27 @@
+import styles from './Post.module.css';
+
+export function Post() {
+    return (
+        <article className={styles.post}>
+            <header>
+                <div className={styles.author}>
+                    <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/30728761?s=96&v=4"/>
+                    <div className={styles.authorInfo}>
+                        <strong>Danilo Martins</strong>
+                        <span>web developer</span>
+                    </div>
+                </div>
+                <time title="21 de Outubro às 08:30" dateTime="2022-10-10 08:13:38">publica há 1 hora</time>
+            </header>
+            <div className={styles.content}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p> 
+                <p>Incidunt ipsum eveniet eius modi iste quasi veniam velit </p>
+                <p>sapiente minima accusamus, earum impedit tempora rem corrupti alias dolorum explicabo dicta cum.</p>
+                
+                <p> <a href=''>Jane.design/doctorcare</a></p>
+                <p><a href="">#novoprojeto #nlu #rocketseat</a></p>
+            </div>
+        </article>
+    )
+
+}
