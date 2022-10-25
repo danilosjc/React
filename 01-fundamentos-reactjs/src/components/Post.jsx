7922423b4a053247830f16c479b1bf5d077bee03
danilosjc <danfonmar@gmail.com>
2022-10-25 14:02:04 -0300

    formulario comentarios

--- 01-fundamentos-reactjs/src/components/Post.jsx
+++ 01-fundamentos-reactjs/src/components/Post.jsx
@@ -21,6 +21,15 @@ export function Post() {
                 <p> <a href=''>Jane.design/doctorcare</a></p>
                 <p><a href="">#novoprojeto #nlu #rocketseat</a></p>
             </div>
+
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea
+                    placeholder='Deixe um comentário'
+                />
+                <button type="submit">Publicar</button>
+            </form>
         </article>
     )
 
