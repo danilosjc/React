2207f6f133181ba1ac8350695fddc8d0545193f2
danilosjc <danfonmar@gmail.com>
2022-10-25 15:42:39 -0300

    comentarios

---
+++ 01-fundamentos-reactjs/src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './comment.module.css';
+
+export function Comment() {
+    return (
+        <div className={styles.comment}>
+            <img src="https://avatars.githubusercontent.com/u/30728761?s=96&v=4" alt="" />
+            
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Danilo Martins</strong>
+                            <time title="21 de Outubro às 08:30" dateTime="2022-10-10 08:13:38">publica cerca de 1h atrás</time>
+                        </div>
+                        <button title='Deletar comentário'>
+                            <Trash size={20}/>
+                        </button>
+                    </header>
+                    <p>Muito bom Dev, parabéns </p>
+                </div>
+
+                <footer>
+                    <button>
+                        <ThumbsUp />
+                        aplaudir <span>20</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
